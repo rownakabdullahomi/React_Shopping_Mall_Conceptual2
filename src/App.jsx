171e9c0bf02b82007fc5d635dbbaee2c5cd876da
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-24 14:02:57 +0600

    button toggle feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import AllProducts from "./Components/AllProducts";
 import CartContainer from "./Components/CartContainer";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const [isActive, setIsActive] = useState({
+    status: "cart",
+  });
+
+  const handleIsActiveState = (status) => {
+    status === "cart"? setIsActive({status: "cart"}):setIsActive({status: "about"})
+  };
+
+  console.log(isActive);
+
   return (
     <>
       <Navbar></Navbar>
       <div className="flex justify-between">
         <AllProducts></AllProducts>
-        <CartContainer></CartContainer>
+        <CartContainer handleIsActiveState={handleIsActiveState}></CartContainer>
       </div>
     </>
   );
--- src/Components/CartContainer.jsx
+++ src/Components/CartContainer.jsx
@@ -1,14 +1,23 @@
-
-const CartContainer = () => {
-    return (
-        <div>
-            <h1>Cart Container.jsx</h1>
-            <div className="toggle flex justify-between">
-                <div className="cart-btn btn">Cart</div>
-                <div className="about-btn btn">About</div>
-            </div>
+const CartContainer = ({ handleIsActiveState }) => {
+  return (
+    <div>
+      <h1>Cart Container.jsx</h1>
+      <div className="toggle flex justify-between">
+        <div
+          onClick={() => handleIsActiveState("cart")}
+          className="cart-btn btn"
+        >
+          Cart
         </div>
-    );
+        <div
+          onClick={() => handleIsActiveState("about")}
+          className="about-btn btn"
+        >
+          About
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartContainer;
